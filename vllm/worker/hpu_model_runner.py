7576bf8d0670cc29d030903c24e55df5d8adc625
Artur Fierka <afierka@habana.ai>
2025-01-24 09:45:46 +0200

    Add compile_one_hot flag condition

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -306,7 +306,10 @@ class HpuModelAdapter:
         attn_bias = (torch.zeros_like(mask, dtype=dtype).masked_fill_(
             mask, -math.inf))
 
-        if not is_fake_hpu():
+        if (is_fake_hpu() and htorch.utils.internal.is_lazy())\
+              or "compile_one_hot" in enabled_flags():
+            # Use one_hot directly on HPU on lazy or on t.compile when
+            # build is >= 20.0.0.370
             block_mapping = torch.nn.functional.one_hot(metadata.block_groups,
                                                         num_classes=batch_size)
         else:
